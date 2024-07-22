36f92023f1a5228d8baf9cbf8f5522f074143ea6
pmk248 <pinchaskimel@gmail.com>
2024-07-22 12:18:36 +0300

    Todos.cs functions

--- Todos.cs
+++ Todos.cs
@@ -21,32 +21,57 @@ namespace TodoList
             this.repository = repository;
         }
 
-        private void populateViewWithTodo() { }
+        private void populateViewWithTodo()
+        {
+            todos.Clear();
+            foreach (var todo in repository.GetAll())
+            {
+                todos.Add(todo);
+            }
+        }
 
         // populate form from selected row
         private void dataGridView_tasks_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-
+            if (e.RowIndex <= 0)
+            {
+                var selectedTodo = todos[e.RowIndex];
+                textbox_title.Text = selectedTodo.Title;
+                hopeDatePicker1.Date = selectedTodo.Date.ToDateTime(TimeOnly.MinValue);
+                SetMode(mode);
+            }
         }
 
         private void SetMode(Mode mode)
         {
             this.mode = mode;
-            switch (mode)
-            {
-                case Mode.Add:
-                    button_action.Text = "Add";
-                    break;
-                case Mode.Edit:
-                    button_action.Text = "Edit";
-                    break;
-            }
+            button_action.Text = mode == Mode.Add ? "Add" : "Edit";
         }
 
         // add or edit based on mode
         private void button_action_Click(object sender, EventArgs e)
         {
-
+            if (mode == Mode.Add)
+            {
+                DateTime dt = hopeDatePicker1.Date;
+                DateOnly d = new DateOnly(dt.Year, dt.Month, dt.Day);
+                var newTodo = new TodoModel(textbox_title.Text, d);
+            }
+            else if (mode == Mode.Edit)
+            {
+                var selectedTodo = todos[dataGridView_tasks.CurrentRow.Index];
+                selectedTodo.Title = textbox_title.Text;
+                selectedTodo.Date = DateOnly.FromDateTime(hopeDatePicker1.Date);
+                repository.Update(selectedTodo);
+            }
+            populateViewWithTodo();
+            SetMode(Mode.Add);
+            ClearForm();
+        }
+        private void ClearForm()
+        {
+            textbox_title.Clear();
+            hopeDatePicker1.Date = DateTime.Today;
         }
     }
 }
