f14bc1a70f2e0b206060ee8989b273646c1b9510
meniNaft <mnaft404@gmail.com>
2024-07-22 10:32:13 +0300

    Update TodoModel.cs

--- TodoModel.cs
+++ TodoModel.cs
@@ -5,25 +5,29 @@ namespace TodoList
     [XmlRoot("Todo")] 
     public class TodoModel
     {
+        public TodoModel(string title, DateOnly date)
+        {
+            Id = counter++;
+            Title = title;
+            Date = date;
+        }
+
+        static private int counter = 1; 
+        
         [XmlElement("ID")] 
         public int Id { get; set; }
 
         [XmlElement("Title")] 
         public string Title { get; set; }
 
-        [XmlElement("Date")] 
-        public string XmlDate
-        {
-            get { return Date.ToString("yyyy-MM-dd"); } 
-            set { Date = DateOnly.Parse(value); } 
-        }
+        //[XmlElement("Date")] 
+        //public string XmlDate
+        //{
+        //    get { return Date.ToString("yyyy-MM-dd"); } 
+        //    set { Date = DateOnly.Parse(value); } 
+        //}
 
         [XmlIgnore] 
         public DateOnly Date { get; set; }
-
-        public TodoModel()
-        {
-
-        }
     }
 }
